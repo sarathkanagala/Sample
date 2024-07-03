3f1a47691c17bee63ea77668366942aea1313922
kanagalasarath <kanagalasarath@kanagalas-mbp.attlocal.net>
2024-07-03 17:17:17 -0500

    First Commit

---
+++ src/main/java/com/thinkconstructive/rest_demo/controller/CloudVendorAPIService.java
@@ -0,0 +1,45 @@
+package com.thinkconstructive.rest_demo.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thinkconstructive.rest_demo.model.CloudVendor;
+
+@RestController
+@RequestMapping("/cloudvendor")
+
+public class CloudVendorAPIService {
+
+	CloudVendor cloudVendor;
+
+	@GetMapping("{vendorId}")
+	public CloudVendor getCloudVendorDetails(String VendorID) {
+		// return cloudVendor;
+		return new CloudVendor("C1", "Vendor 1", "Address One", "xxxx");
+
+	}
+
+	@PostMapping
+	public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor Created Successfully";
+	}
+
+	@PutMapping
+	public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor Updated Successfully";
+	}
+
+	@DeleteMapping("{vendorId}")
+	public String deleteCloudVendorDetails(String vendorId) {
+		this.cloudVendor = null;
+		return "Cloud Vendor Deleted Successfully";
+	}
+
+}
